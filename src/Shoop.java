ef0a2c8c3a1d16ed6193f3afb4ed839e21c45a1c
ThirdTime <league@WorthyMini-2.local>
2017-05-06 12:19:18 -0700

    Made the Scoop do more things! Like multiply or get a trim
    5/6/17

--- src/Shoop.java
+++ src/Shoop.java
@@ -1,32 +1,81 @@
+import java.util.Random;
+
 import org.omg.Messaging.SyncScopeHelper;
 
 public class Shoop {
 	private int tailLength;
 	private String shape;
+	private int numCalled = 2;
 
 	public Shoop(int tailLength, String shape) {
 		this.tailLength = tailLength;
 		this.shape = shape;
-		System.out.println("Created Shoop with tail length " + tailLength+ ".");
+		System.out.println("Created Shoop with tail length " + tailLength + " and shape " + shape + ".");
 	}
 
 	public void lengthenTail(int inches) {
 		this.tailLength = this.tailLength + inches;
-		System.out.println("You have extended your shoop's tail "+ inches+ " inches!");
-		System.out.println("Your shoop now has a tail length of "+tailLength+ " inches.");
+		System.out.println("You have extended your shoop's tail " + inches + " inches!");
+		System.out.println("Your shoop now has a tail length of " + tailLength + " inches.");
 	}
 
-	/*
-	 * public void trimShoop(String "shape"){ if
-	 * shape.equalsIgnoreCase("Square"){ System.out.println(
-	 * "Your shoop is now a square!"); }
-	 * 
-	 * }
-	 */
+	public void trimShoop(String newshape) {
+		System.out.println("Your shoop is now trimmed! It's now a " + newshape);
+	}
+
+	public void multiply() {
+		if (numCalled == 2) {
+			System.out.println("You now have shoopers! That's " + numCalled + " shoop.");
+			numCalled = numCalled * 2;
+		} else if (numCalled == 4) {
+			System.out.println("You now have shoopith!  That's " + numCalled + " shoop.");
+			numCalled = numCalled * 2;
+		} else if (numCalled == 8) {
+			System.out.println("You now have shoopen! That's " + numCalled + " shoop.");
+			numCalled = numCalled * 2;
+		} else if (numCalled == 16) {
+			System.out.println("You now have shoopid! That's " + numCalled + " shoop.");
+			numCalled = numCalled + 1;
+		} else if (numCalled >= 17) {
+			System.out.println("Too far! Stop multiplying!");
+
+		}
+	}
+
+	public void countShoop() {
+		if (numCalled <= 16) {
+			System.out.println("You have " + numCalled + " shoop.");
+		} else {
+			System.out.println("You made too many shoop.");
+		}
+	}
+
+	public void haircut() {
+		int rannum = new Random().nextInt(3) + 1;
+		if (rannum == 1) {
+			System.out.println("Your shoop now has a mohawk!");
+		}
+		if (rannum == 2) {
+			System.out.println("Your shoop now has a mullet!");
+		}
+		if (rannum == 3) {
+			System.out.println("Your shoop now has an afro!");
+		}
+	}
 
 	public static void main(String[] args) {
-		Shoop Tim = new Shoop(3, "triangle");
+		Shoop bob = new Shoop(5, "shoop");
+		/* Here's the things you can do to the Shoop!
+		 * You can lengthen the tail length of your Shoop with shoop.lengthenTail([number of inches]);
+		 * You can change the shape of your Shoop with shoop.trimShoop([new shape]);
+		 * You can multiply your Shoop up to four times with shoop.multiply();
+		 * You can count how many Shoop you have with shoop.countShoop(); [incomplete]
+		 * You can give your Shoop a random haircut with shoop.haircut(); [incomplete]
+		 */
 
-		Tim.lengthenTail(5);
+		bob.multiply();
+		bob.countShoop();
+		bob.trimShoop("square");
+		bob.haircut();
 	}
 }
