79096d2b492916d522489b40acc51273eef23c54
DemoliciousDt <league@Thes-Mac-mini.local>
2017-08-26 12:24:11 -0700

    Finished CutenessTV, happy birthday to me
    8/26/17

--- src/CutenessTV.java
+++ src/CutenessTV.java
@@ -11,48 +11,71 @@ import javax.swing.JPanel;
 */
 
 public class CutenessTV implements ActionListener {
-
-	
-	
+	JButton dogButton = new JButton();
+	JButton frogButton = new JButton();
+	JButton duckButton = new JButton();
 
 	public void actionPerformed(ActionEvent event) {
 		JButton buttonPressed = (JButton) event.getSource();
+		if(dogButton == buttonPressed){
+			showDog();
+		}
+		if(frogButton == buttonPressed){
+			showFrog();
+		}
+		if(duckButton == buttonPressed){
+			showDuck();
+		}
+		System.out.println("action performed is working");
+	}
 
+	public void createUI() {
+		duckButton.addActionListener(this);
+		dogButton.addActionListener(this);
+		frogButton.addActionListener(this);
+
+		JPanel panel = new JPanel();
+		JFrame frame = new JFrame();
+
+		frame.setVisible(true);
+		frame.setSize(400, 400);
+		frame.add(panel);
+		panel.add(duckButton);
+		panel.add(dogButton);
+		panel.add(frogButton);
+		dogButton.setText("Dogs!");
+		frogButton.setText("Frogs!");
+		duckButton.setText("Ducks!");
+		frame.pack();
 	}
 
 	void showDog() {
-		playVideo("https://www.youtube.com/watch?v=ccgkxP-4tVE");
+		playVideo("ccgkxP-4tVE");
 	}
 
 	void showFrog() {
 		playVideo("aSjCb-FfxhI");
 	}
 
-	void showFluffyUnicorns() {
-		playVideo("qRC4Vk6kisY");
+	void showDuck() {
+		playVideo("e3PdcKcUvDY&t=16s");
 	}
 
 	void playVideo(String videoID) {
 		try {
-			URI uri = new URI("https://www.youtube.com/v/" + videoID + "?autoplay=1");
+			URI uri = new URI("https://www.youtube.com/watch?v=" + videoID + "?autoplay=1");
 			java.awt.Desktop.getDesktop().browse(uri);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
-
+	
+	
 
 	public static void main(String[] args) {
-		JPanel panel = new JPanel();
-		JFrame frame = new JFrame();
-		JButton dogButton = new JButton();
+
+		new CutenessTV().createUI();
 		
-		frame.setVisible(true);
-		frame.setSize(400, 400);
-		frame.add(panel);
-		panel.add(dogButton);
-		dogButton.setText("Dogs!");
-		frame.pack();
+
 	}
 }
